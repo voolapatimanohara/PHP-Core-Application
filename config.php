fee7064662ed7df0308118b46bb3f71a8684e422
unknown <voolapatimanohara@gmail.com>
2022-01-21 14:35:43 +0530

    formatted doc

--- config.php
+++ config.php
@@ -1,17 +1,15 @@
 <?php
 
-   $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "info_project";  
-      
-    $db = mysqli_connect($host, $user, $password, $db_name); 
+$host = "localhost";
+$user = "root";
+$password = '';
+$db_name = "info_project";
+
+$db = mysqli_connect($host, $user, $password, $db_name);
 if (!$db) {
 
     echo "Connection failed!";
-
-}	
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
-?>
+}
+if (mysqli_connect_errno()) {
+    die("Failed to connect with MySQL: " . mysqli_connect_error());
+}
